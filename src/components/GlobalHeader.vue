006d534b4442278915063595068afcbd6235ef8a
Epiphany0130 <sdguyq@163.com>
2025-12-27 10:53:14 +0800

    图片上传创建页面 + 图片信息修改

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -68,6 +68,11 @@ const originItems = [
     title: '关于',
   },
   {
+    key: '/add_picture',
+    label: '创建图片',
+    title: '创建图片',
+  },
+  {
     key: '/admin/userManage',
     label: '用户管理',
     title: '用户管理',
